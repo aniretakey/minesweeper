9873f0af83840296da8d052123a0d0f4c4733643
aniretakey <EMKornishina@gmail.com>
2023-05-18 15:28:54 +0300

    fix: add addTheme to index.js file

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,10 +1,12 @@
 import { createMarkup } from './modules/createMarkup.js';
-import { createField } from './modules/createField.js';
+import { createField, matrix } from './modules/createField.js';
+import { changeTheme } from './modules/changeTheme.js';
 
 createMarkup();
 
 function newGame() {
   createField();
+  changeTheme();
 }
 
 newGame();
