2b7a3a4c2df36e2a4fa73a6c9e7ece792c292b67
aniretakey <EMKornishina@gmail.com>
2023-05-20 21:38:04 +0300

    feat: add color coding for numbers

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,12 +1,14 @@
 import { createMarkup } from './modules/createMarkup.js';
 import { createField, matrix } from './modules/createField.js';
 import { changeTheme } from './modules/changeTheme.js';
+import { getColors } from './modules/numColors.js';
 
 createMarkup();
 
 function newGame() {
   createField();
   changeTheme();
+  getColors();
 }
 
 newGame();
---
+++ minesweeper/modules/numColors.js
@@ -0,0 +1,54 @@
+export function getColors() {
+  const field = document.getElementById('field');
+  field.addEventListener('click', (event) => {
+    let btn = event.target.closest('button');
+    console.log(btn.classList);
+    if (btn.classList.contains('bomb')) {
+      btn.classList.add('open');
+      btn.innerHTML = '<img src="./assets/icons/bomb4.png">';
+      console.log('BOOOM! YOU LOOSE!');
+    } else {
+      btn.classList.add('open');
+      const bombCount = btn.classList[1];
+      console.log(btn.classList[1]);
+
+      if (btn.classList[1] == 0) {
+        btn.innerHTML = '';
+      } else {
+        btn.innerHTML = `${btn.classList[1]}`;
+
+        if (btn.classList[1] == 1) {
+          btn.classList.add('blue');
+        }
+
+        if (btn.classList[1] == 2) {
+          btn.classList.add('green');
+        }
+
+        if (btn.classList[1] == 3) {
+          btn.classList.add('red');
+        }
+
+        if (btn.classList[1] == 4) {
+          btn.classList.add('darkblue');
+        }
+
+        if (btn.classList[1] == 5) {
+          btn.classList.add('brown');
+        }
+
+        if (btn.classList[1] == 6) {
+          btn.classList.add('cyan');
+        }
+
+        if (btn.classList[1] == 7) {
+          btn.classList.add('black');
+        }
+
+        if (btn.classList[1] == 8) {
+          btn.classList.add('grey');
+        }
+      }
+    }
+  });
+}
