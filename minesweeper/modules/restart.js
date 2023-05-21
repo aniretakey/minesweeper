2f5dced7dac40e3ef02e6d9eac030273df913432
aniretakey <EMKornishina@gmail.com>
2023-05-22 00:25:00 +0300

    fix: update restart function

--- minesweeper/modules/restart.js
+++ minesweeper/modules/restart.js
@@ -2,14 +2,10 @@ import { newGame } from '../index.js';
 
 export function restart() {
   let restartButton = document.querySelector('.restart');
-  const field = document.getElementById('field');
-  const timer = document.querySelector('.timer');
+  const body = document.getElementsByTagName('body')[0];
 
   restartButton.addEventListener('click', (event) => {
-    field.innerHTML = '';
-    timer.innerHTML = '0';
-    //need a function to restart timer
-
+    body.innerHTML = '';
     newGame();
   });
 }
