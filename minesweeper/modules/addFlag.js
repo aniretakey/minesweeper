895f266508ca754f064cc3035ba42fe8560a80f5
aniretakey <EMKornishina@gmail.com>
2023-05-21 18:20:04 +0300

    feat: add separate function for addFlags

---
+++ minesweeper/modules/addFlag.js
@@ -0,0 +1,30 @@
+export function addFlag() {
+  const field = document.getElementById('field');
+
+  const flag = new Audio();
+  flag.src = '/assets/audio/flag.mp3';
+  flag.volume = '0.08';
+
+  field.addEventListener('contextmenu', (event) => {
+    let btn = event.target.closest('button');
+
+    event.preventDefault();
+
+    if (btn) {
+      if (!btn.classList.contains('open')) {
+        if (btn.classList.contains('flag')) {
+          console.log('remove flag');
+          btn.innerHTML = '';
+          btn.classList.remove('flag');
+          return;
+        } else {
+          console.log('add flag');
+          btn.innerHTML = '<img src="./assets/icons/redflag.png">';
+          flag.play();
+          btn.classList.add('flag');
+          return;
+        }
+      }
+    }
+  });
+}
