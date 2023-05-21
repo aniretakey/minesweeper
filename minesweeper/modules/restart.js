5dd00f0d75690f959e4a5e7d4841aaf78bf60b4c
aniretakey <EMKornishina@gmail.com>
2023-05-21 14:24:04 +0300

    feat: add func for restart game

--- minesweeper/modules/restart.js
+++ minesweeper/modules/restart.js
@@ -3,9 +3,13 @@ import { newGame } from '../index.js';
 export function restart() {
   let restartButton = document.querySelector('.restart');
   const field = document.getElementById('field');
+  const timer = document.querySelector('.timer');
 
   restartButton.addEventListener('click', (event) => {
     field.innerHTML = '';
+    timer.innerHTML = '0';
+    //need a function to restart timer
+
     newGame();
   });
 }
