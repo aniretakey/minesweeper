17c280d867c7c6513d49666f71fd09abe7a6348b
aniretakey <EMKornishina@gmail.com>
2023-05-21 21:52:31 +0300

    fix: update func for create bomb arr

--- minesweeper/modules/createBombs.js
+++ minesweeper/modules/createBombs.js
@@ -4,8 +4,9 @@ export function createBombs(bombs = 10) {
   let arr = [];
   for (let i = 1; i <= bombs; i += 1) {
     let bombNum = getRandomNumbers();
-    if (arr.includes(bombNum)) continue;
-    arr.push(bombNum);
+    if (!arr.includes(bombNum)) {
+      arr.push(bombNum);
+    } else return;
   }
   return arr;
 }
